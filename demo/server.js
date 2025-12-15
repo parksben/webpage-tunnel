cf874e6b6ed6145a2d8baaf56c07901e7b6ddaf1
pengan <pengan@stepfun.com>
2025-12-16 01:33:49 +0800

    1.2.0

--- demo/server.js
+++ demo/server.js
@@ -1,7 +1,7 @@
-import http from 'node:http';
-import fs from 'node:fs';
-import path from 'node:path';
 import { exec } from 'node:child_process';
+import fs from 'node:fs';
+import http from 'node:http';
+import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 
 const __filename = fileURLToPath(import.meta.url);
